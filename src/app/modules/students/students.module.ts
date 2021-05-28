58537db212b3d419e6edb2d5f99e95fcd2056e23
shubhendu <shubhendukumars09@gmail.com>
2021-05-28 23:58:01 +0530

    students components &module created

---
+++ src/app/modules/students/students.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StudentComponent } from './student/student.component';
+import { StudentListComponent } from './student-list/student-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    StudentComponent,
+    StudentListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class StudentsModule { }
